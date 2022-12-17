d6ce09c1605825f68a9524e7c5e7776f2ce2cd41
arunkudiyal <arunkudiyal@icloud.com>
2022-12-17 12:28:58 +0530

    4 Pillars of OOPs - Encapsulation & Abstraction

--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -41,3 +41,73 @@
 # print( cow.animalIsCarnivorous )
 
 
+# JS -> Number -> methods
+
+# 1. Encapsulation --> You are supposed to create a single unit, a unit conatining the attributes and methods, which is called a class
+class Number:
+    # Attribute | Data Members | Properties
+    # Constructor -> i. Default Constructors [Without arguments] ii. Parameterized Constructor
+    # def __init__(self): ----
+    def __init__(self, num1, num2):
+        # 2. Abstraction --> A way to ony give user details which are necessary, and hide the implementation detail as much possible
+        self.__sum = 0
+        self.num1 = num1
+        self.num2 = num2
+
+    # Methods --> Functionalities which you want to provide to your objects
+    # There should be no method with the same name but different arguments.
+    def sum(self):
+        self.__sum = self.num1 + self.num2
+        return self.__sum
+
+    def otherTypeOfSum(self, num3):
+        return self.num1 + self.num2 + num3
+
+    def difference(self):
+        return self.num1 - self.num2
+
+    def product(self):
+        return self.num1 * self.num2
+    
+    def divide(self):
+        return self.num1 / self.num2
+
+    def power(self):
+        return self.num1 ** self.num2
+
+    def modulous(self):
+        return self.num1 % self.num2
+
+    def completeDivide(self):
+        return self.num1 // self.num2
+
+# Create the objects
+# Access the attributes of the class
+myNumbers = Number(100, 2)
+print( myNumbers.num1 )
+print( myNumbers.num2 )
+print( myNumbers.sum() )
+
+# myNumbers = Number(100, 2)
+# print( 'The sum is - {}'.format(myNumbers.otherTypeOfSum(100)) )
+# print( 'The sum is - {}'.format(myNumbers.sum()) )
+# print( 'The difference is - {}'.format(myNumbers.difference()) )
+# print( 'The product is - {}'.format(myNumbers.product()) )
+# print( 'The division is - {}'.format(myNumbers.divide()) )
+# print( 'The modulous is - {}'.format(myNumbers.modulous()) )
+# print( 'The completeDivide is - {}'.format(myNumbers.completeDivide()) )
+# print('---------- * ----------')
+# myNumbersTwo = Number(20, 2)
+# print( 'The sum is - {}'.format(myNumbersTwo.sum()) )
+# print( 'The difference is - {}'.format(myNumbersTwo.difference()) )
+# print( 'The product is - {}'.format(myNumbersTwo.product()) )
+# print( 'The division is - {}'.format(myNumbersTwo.divide()) )
+# print( 'The modulous is - {}'.format(myNumbersTwo.modulous()) )
+# print( 'The completeDivide is - {}'.format(myNumbersTwo.completeDivide()) )
+
+
+# 4 Pillars of OOPs
+# 1. Encapsulation
+# 2. Abstraction
+# 3. Polymorphism
+# 4. Inheritance
